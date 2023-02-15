172013c40f5f72609727a5f6eff39c62290407fb
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-02-15 22:11:21 +0300

    Update
    API connection made and created user detail.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Home from './components/Home'
 import About from './components/About'
 import Users from './components/Users'
+import User from './components/User'
 
 function App() {
   return (
@@ -27,9 +28,10 @@ function App() {
           {/* A <Switch> looks through its children <Route>s and
               renders the first one that matches the current URL. */}
           <Switch>
+            <Route path="/" exact component={Home} />
             <Route path="/about" component={About} />
             <Route path="/users" component={Users} />
-            <Route path="/" component={Home} />
+            <Route path="/user/:id" component={User} />
           </Switch>
         </div>
       </Router>
