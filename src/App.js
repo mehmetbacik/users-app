d4cbc956e5ea8f1538e7294a452f3942965e74ef
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-02-16 23:11:59 +0300

    Update
    Page 404 was created.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route, NavLink} from "react-router-dom
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
+import Error404 from './components/Error404';
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
             <Route path="/" exact component={Home} />
             <Route path="/about" component={About} />
             <Route path="/users" component={Users} />
+            <Route path="*" component={Error404} />
           </Switch>
         </div>
       </Router>
