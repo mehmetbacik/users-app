930bbbc6e55f9dd435f961b50052ff9a69e3ea99
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-02-22 23:19:20 +0300

    Update
    Updated users app for style.

--- src/components/User.js
+++ src/components/User.js
@@ -12,12 +12,18 @@ function User() {
         ).finally(() => setLoading(false));
     }, [id]);
     return (
-        <div>
-            <h1>User Detail</h1>
-            {loading && <div>Loading...</div>}
-            {!loading && <code>{JSON.stringify(user)}</code>}
-            <br></br>
-            <Link to={`/users/${parseInt(id) + 1}`}>Next User ({parseInt(id) + 1})</Link>
+        <div id='users__app__userdetail'>
+            <div className='headline'>
+                <h1>User Detail</h1>
+            </div>
+            <div className='detail'>
+                {loading && <div>Loading...</div>}
+                {!loading && <code>{JSON.stringify(user)}</code>}
+            </div>
+            <div className='next__button'>
+                <Link to={`/users/${parseInt(id) + 1}`}>Next User ({parseInt(id) + 1})</Link>
+            </div>
+            
         </div>
     );
 }
